5c3e639a6dcbd794fb720f330ad534ca921706ec
Monica Kornis <monicakornis@Monicas-MBP-2.lan>
2023-04-05 23:56:18 -0400

    adding other pages

--- src/Services.js
+++ src/Services.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import  styled  from 'styled-components'
+import computerSrc from './assets/keyboard.jpg'
+
+
+const BannerImage = styled.img`
+  width: 100%;
+  @media screen and (max-width: 600px) {
+    width: 170%;
+  }
+`;
+
+const BannerText = styled.p`
+  color: white;
+  font-family: 'Raleway';
+  font-size: 56px;
+  font-weight: 600;
+  text-shadow: rgb(0 0 0) 3px 7px 20px;
+  @media screen and (max-width: 600px) {
+    font-size: 36px;
+  }
+`;
+
+const BannerContainer = styled.div`
+  position: relative;
+  height: 300px;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+`;
+
+const BannerContent = styled.div`
+margin: auto;
+left: 0;
+right: 0;
+text-align: center;
+width: 79%;
+position: absolute;
+`;
 
 function Services() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <BannerContainer>
+            <BannerContent>
+        </BannerContent>
+        <BannerImage src={computerSrc} alt="computer-repair-image" />
+      </BannerContainer>
         <p>
           SERVICES
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
